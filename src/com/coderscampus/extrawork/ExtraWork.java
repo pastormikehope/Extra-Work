a8bbf4de682625159b8dc41d3cf4b87eb7d7a5a5
pastormikehope <155399156+pastormikehope@users.noreply.github.com>
2024-04-21 21:21:50 -0400

    Initial commit

---
+++ src/com/coderscampus/extrawork/ExtraWork.java
@@ -0,0 +1,27 @@
+package com.coderscampus.extrawork;
+
+import java.util.Scanner;
+
+
+public class ExtraWork {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Who is the best NBA player of all time?");
+		String nbaPlayer = scanner.nextLine();
+		
+		if (nbaPlayer.equals("Steph Curry")) {
+			System.out.println("You're Right");
+			
+		} else {
+			System.out.println("What?  It's Steph Curry!!");
+			
+			
+		}
+        scanner.close();
+        
+        
+	}
+
+}
